5f56ecc57cabd5da01825aaffd75f9b62b5df6bc
JackelineGCP <gessenia_17@hotmail.com>
2018-02-14 09:02:37 -0500

    agregando estilo

--- public/js/app.js
+++ public/js/app.js
@@ -12,24 +12,25 @@ form.submit(function (e) {
 
 function getNews() {
   $.ajax({
-    url:`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=c685e23f34b74c619a0895e03bf08727`
-  }).done(addNews)
-  .fail(handleError);
+      url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=c685e23f34b74c619a0895e03bf08727`
+    }).done(addNews)
+    .fail(handleError);
 }
 
-function handleError(){
+function handleError() {
   console.log('Se ha presentado un error');
 }
 
-function addNews(news){
+function addNews(news) {
   const articles = news.response.docs;
 
-  articles.forEach(function(article){
+  articles.forEach(function (article) {
     const title = article.headline.main;
     const snippet = article.snippet;
-
+    
+    
     let li = $('<li />').text(snippet);
-
+    li.className = 'li'
     responseContainer.append(li);
   })
 }
